fe0f0089b89de33315dd60f9b617c510d5dd35ce
Nicsena <Nicholassena8190@gmail.com>
2023-05-21 12:12:36 -0700

    Search & Two New Endpoints

--- server.js
+++ server.js
@@ -12,11 +12,16 @@ const env = process.env
 var port = env.PORT || 3000
 var apptitle = env.APP_TITLE || "YouTube Proxy"
 var resultLimit = env.RESULT_LIMIT || 25
+var useragent = env.USER_AGENT || "googlebot"
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.enable("trust proxy");
 
+app.use(function (req, res, next) {
+    res.setHeader('X-Content-Type-Options', "nosniff");
+    next();
+  });
 
 app.get("/", (req, res) => {
     res.render(path.join(__dirname, "/views/index"), {
@@ -28,32 +33,73 @@ app.get("/search", async (req, res) => {
     var query = req.query.q
     var pageNum = parseInt(req.query.p || 1)
     if(!query) return res.redirect("/");
-    if(typeof pageNum !== "number") return res.status(400).send("Page is not a number")
+
+    if(typeof pageNum !== "number") {
+        console.log("The type of the pageNum variable is being detected as a string, not a number.");
+        res.status(400).render("error.ejs", {
+            apptitle: apptitle,
+            title: "400 - Bad Request",
+            content: `The param is being detected as a string, not a number.`,
+        });
+    };
 
     try {
-        var searchResults = await ytsr(query, { resultLimit, pages: pageNum })
-        res.render(path.join(__dirname, "/views/search"), {
+        var searchResult = await ytsr(query, { resultLimit, pages: pageNum })
+        res.status(200).render(path.join(__dirname, "/views/search"), {
             apptitle: apptitle,
-            result: searchResults["items"],
+            result: searchResult["items"],
+            resultNum: searchResult["results"],
             query: query,
             page: pageNum
         });
     } catch(e) {
         console.error(e);
-        res.render(path.join(__dirname, "/views/error"), {
+        res.status(400).render(path.join(__dirname, "/views/error"), {
             apptitle: apptitle,
             title: "Error",
             content: e
         });
     };
+
+});
+
+app.get(["/vi*", "/sb/*"], async (req, res) => {
+    var url = "https://i.ytimg.com" + req.url
+    var options = { headers: { "User-Agent": useragent }, isStream: true, maxRedirects: 2 };
+
+    try {
+        var request = await got.get(url, options)
+        request.pipe(res);
+        //res.status(200).send("OK")
+    } catch(e) {
+        console.error(e);
+        res.status(500).json({ message: "Internal Server Error", error: e})
+    }
+
+});
+
+app.get(["/yt3/*", "/ytc/*"], async (req, res) => {
+    if (req.url.startsWith("/yt3/")) req.url = req.url.slice(4);
+    var url = "https://yt3.ggpht.com" + req.url
+    var options = { headers: { "User-Agent": useragent }, isStream: true, maxRedirects: 2 };
+
+    try {
+        var request = await got.get(url, options)
+        request.pipe(res);
+        //res.status(200).send("OK")
+        } catch(e) {
+        console.error(e);
+        return res.status(500).json({ message: "Internal Server Error", error: e});
+    }
+
 });
 
 
-app.get("*", (req, res) => {
-    res.render(path.join(__dirname, "/views/404"), {
+app.use((req, res) => {
+    res.status(404).render("error.ejs", {
         apptitle: apptitle,
-        title: "404",
-        content: "Page Not Found"
+        title: "404 - Not Found",
+        content: "Page Not Found",
     });
 });
 
